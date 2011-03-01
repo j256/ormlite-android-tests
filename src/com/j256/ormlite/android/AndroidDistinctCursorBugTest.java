fc4b25091b3de338562e32b413656045e6e31aff
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2011-03-01 13:24:42 +0000

    Formatting and comments.

--- src/com/j256/ormlite/android/AndroidDistinctCursorBugTest.java
+++ src/com/j256/ormlite/android/AndroidDistinctCursorBugTest.java
@@ -3,9 +3,9 @@ package com.j256.ormlite.android;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteStatement;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.test.AndroidTestCase;
 import android.util.Log;
 
