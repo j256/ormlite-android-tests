b2be306290029af889925179ebcf2aefb8beab54
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2011-03-22 18:57:19 +0000

    Big check in with initial support for @ForeignCollectionField annotation and associated support.  Also added a DaoManager for caching DAOs.

--- src/com/j256/ormlite/android/OrmDatabaseHelper.java
+++ src/com/j256/ormlite/android/OrmDatabaseHelper.java
@@ -1,16 +1,10 @@
 package com.j256.ormlite.android;
 
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
-
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
-import com.j256.ormlite.dao.BaseDaoImpl;
-import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.support.ConnectionSource;
 
 public class OrmDatabaseHelper extends OrmLiteSqliteOpenHelper {
@@ -23,8 +17,6 @@ public class OrmDatabaseHelper extends OrmLiteSqliteOpenHelper {
 	 * The version number of the database. This allows us to do conversions from various older versions to newer.
 	 */
 	private static final int DATABASE_VERSION = 1;
-
-	private final Map<Class<?>, Dao<?, ?>> daoMap = new HashMap<Class<?>, Dao<?, ?>>();
 
 	OrmDatabaseHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -39,23 +31,6 @@ public class OrmDatabaseHelper extends OrmLiteSqliteOpenHelper {
 	public void onUpgrade(SQLiteDatabase database, ConnectionSource cs, int oldVersion, int newVersion) {
 		Log.i(OrmDatabaseHelper.class.getName(), "onUpgrade from version " + oldVersion + " to version " + newVersion);
 		onCreate(database);
-	}
-
-	/**
-	 * Get a DAO for our class. This stores the DAO in a map to try and cache them.
-	 */
-	public <T, ID> Dao<T, ID> getDao(Class<T> clazz) throws SQLException {
-		synchronized (daoMap) {
-			@SuppressWarnings("unchecked")
-			Dao<T, ID> dao = (Dao<T, ID>) daoMap.get(clazz);
-			if (dao == null) {
-				dao = BaseDaoImpl.createDao(getConnectionSource(), clazz);
-				daoMap.put(clazz, dao);
-			} else {
-				((BaseDaoImpl<T, ID>) dao).setConnectionSource(getConnectionSource());
-			}
-			return dao;
-		}
 	}
 
 	@Override
