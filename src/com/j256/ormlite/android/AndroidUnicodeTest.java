d3df537f67dcd352268b08f0f52df1420da9017e
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2011-09-22 13:01:57 +0000

    More query tests.

--- src/com/j256/ormlite/android/AndroidUnicodeTest.java
+++ src/com/j256/ormlite/android/AndroidUnicodeTest.java
@@ -1,10 +1,15 @@
 package com.j256.ormlite.android;
 
+import java.util.List;
+
 import android.test.AndroidTestCase;
 
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.stmt.QueryBuilder;
+import com.j256.ormlite.stmt.SelectArg;
+import com.j256.ormlite.stmt.Where;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
 
@@ -32,7 +37,7 @@ public class AndroidUnicodeTest extends AndroidTestCase {
 		}
 	}
 
-	public void testStoreChinese() throws Exception {
+	public void testStoreUnicode() throws Exception {
 		TableUtils.dropTable(connectionSource, Foo.class, true);
 		TableUtils.createTable(connectionSource, Foo.class);
 		try {
@@ -50,28 +55,45 @@ public class AndroidUnicodeTest extends AndroidTestCase {
 		}
 	}
 
+	public void testQueryForUnicode() throws Exception {
+		TableUtils.dropTable(connectionSource, Foo.class, true);
+		TableUtils.createTable(connectionSource, Foo.class);
+		try {
+			Dao<Foo, Object> dao = DaoManager.createDao(connectionSource, Foo.class);
+			Foo foo = new Foo();
+			String unicodeString = "上海";
+			foo.stuff = unicodeString;
+			assertEquals(1, dao.create(foo));
+
+			QueryBuilder<Foo, Object> qb = dao.queryBuilder();
+			Where<Foo, Object> where = qb.where();
+			where.eq(Foo.STUFF_FIELD_NAME, unicodeString);
+			List<Foo> results = qb.query();
+			assertNotNull(results);
+			assertEquals(1, results.size());
+			assertEquals(unicodeString, results.get(0).stuff);
+			
+			where.clear();
+			where.eq(Foo.STUFF_FIELD_NAME, new SelectArg(unicodeString));
+			results = qb.query();
+			assertNotNull(results);
+			assertEquals(1, results.size());
+			assertEquals(unicodeString, results.get(0).stuff);
+
+		} finally {
+			TableUtils.dropTable(connectionSource, Foo.class, true);
+		}
+	}
+
 	protected static class Foo {
 		public final static String ID_FIELD_NAME = "id";
 		public final static String STUFF_FIELD_NAME = "stuff";
-		public final static String VAL_FIELD_NAME = "val";
 
 		@DatabaseField(generatedId = true, columnName = ID_FIELD_NAME)
 		public int id;
 		@DatabaseField(columnName = STUFF_FIELD_NAME)
 		public String stuff;
-		@DatabaseField(columnName = VAL_FIELD_NAME)
-		public int val;
 		public Foo() {
-		}
-		@Override
-		public boolean equals(Object other) {
-			if (other == null || other.getClass() != getClass())
-				return false;
-			return id == ((Foo) other).id;
-		}
-		@Override
-		public String toString() {
-			return "Foo.id=" + id;
 		}
 	}
 }
