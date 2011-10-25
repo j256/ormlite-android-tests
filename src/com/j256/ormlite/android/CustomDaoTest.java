98de605e02becd460ddf3d859a59e1df6bafd0cc
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2011-10-25 21:24:10 +0000

    Added test for 4.28 stupid-ness.

--- src/com/j256/ormlite/android/CustomDaoTest.java
+++ src/com/j256/ormlite/android/CustomDaoTest.java
@@ -1,9 +1,15 @@
 package com.j256.ormlite.android;
 
+import java.sql.SQLException;
+
 import android.test.AndroidTestCase;
 
+import com.j256.ormlite.dao.BaseDaoImpl;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.DatabaseTable;
+import com.j256.ormlite.table.TableUtils;
 
 /**
  * We do this to protect from any changes in the future to the way we get the daos from the helper. 4.28 broke this.
@@ -13,14 +19,31 @@ public class CustomDaoTest extends AndroidTestCase {
 	public void testCustomDao() throws Exception {
 		DatabaseHelper helper = new DatabaseHelper(getContext());
 		OurDao ourDao = helper.getDao(Foo.class);
-		Foo foo = new Foo();
-		foo.stuff = "jfpfjewf";
-		assertEquals(1, ourDao.create(foo));
+		TableUtils.dropTable(helper.getConnectionSource(), Foo.class, true);
+		TableUtils.createTable(helper.getConnectionSource(), Foo.class);
+		try {
+			Foo foo = new Foo();
+			foo.stuff = "jfpfjewf";
+			assertEquals(1, ourDao.create(foo));
+
+			Foo result = ourDao.queryForId(foo.id);
+			assertNotNull(result);
+			assertEquals(foo.stuff, result.stuff);
+		} finally {
+			TableUtils.dropTable(helper.getConnectionSource(), Foo.class, true);
+		}
 	}
 
 	public interface OurDao extends Dao<Foo, Integer> {
 	}
 
+	public static class OurDaoImpl extends BaseDaoImpl<Foo, Integer> implements OurDao {
+		public OurDaoImpl(ConnectionSource connectionSource) throws SQLException {
+			super(connectionSource, Foo.class);
+		}
+	}
+
+	@DatabaseTable(daoClass = OurDaoImpl.class)
 	protected static class Foo {
 		@DatabaseField(generatedId = true)
 		int id;
