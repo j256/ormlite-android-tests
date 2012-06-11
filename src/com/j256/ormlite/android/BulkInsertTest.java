59eec3df874183125dc41a2c13cc709b80a7e487
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2012-06-11 18:23:00 +0000

    Cp

---
+++ src/com/j256/ormlite/android/BulkInsertTest.java
@@ -0,0 +1,76 @@
+package com.j256.ormlite.android;
+
+import java.util.concurrent.Callable;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.logger.Logger;
+import com.j256.ormlite.logger.LoggerFactory;
+
+/**
+ * Tests some escaping of names
+ * 
+ * @author graywatson
+ */
+public class BulkInsertTest extends BaseDaoTest {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
+	public void testBulk() throws Exception {
+		final Dao<Foo, Integer> dao = createDao(Foo.class, true);
+
+		logger.info("starting bulk no-batch run");
+
+		long before = System.currentTimeMillis();
+		doInserts(dao);
+		long noBatchTimeMs = System.currentTimeMillis() - before;
+		logger.info("bulk no-batch run finished after {}ms", noBatchTimeMs);
+
+		logger.info("starting bulk batch run");
+		before = System.currentTimeMillis();
+		dao.callBatchTasks(new Callable<Void>() {
+			public Void call() throws Exception {
+				doInserts(dao);
+				return null;
+			}
+		});
+		long batchTimeMs = System.currentTimeMillis() - before;
+		logger.info("bulk batch run finished after {}ms", batchTimeMs);
+		assertTrue(batchTimeMs < noBatchTimeMs);
+	}
+
+	private void doInserts(final Dao<Foo, Integer> dao) throws Exception {
+		for (int i = 0; i < 1000; i++) {
+			Foo foo = new Foo();
+			foo.stuff1 = Integer.toString(i);
+			assertEquals(1, dao.create(foo));
+		}
+	}
+
+	protected static class Foo {
+		@DatabaseField(generatedId = true)
+		int id;
+		@DatabaseField
+		String stuff1;
+		@DatabaseField
+		String stuff2;
+		@DatabaseField
+		String stuff3;
+		@DatabaseField
+		String stuff4;
+		@DatabaseField
+		String stuff5;
+		@DatabaseField
+		String stuff6;
+		@DatabaseField
+		String stuff7;
+		@DatabaseField
+		String stuff8;
+		@DatabaseField
+		String stuff9;
+		@DatabaseField
+		String stuff10;
+		public Foo() {
+		}
+	}
+}
